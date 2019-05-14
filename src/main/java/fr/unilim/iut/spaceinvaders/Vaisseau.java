9817ff6feb09b257125a97a561128116be70d80d
Fougeret <clement.fougeret@etu.unilim.fr>
2019-05-14 11:11:26 +0200

    ajout de vitesse

--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -4,7 +4,8 @@ public class Vaisseau {
 
     private Position origine;
     private Dimension dimension;
-
+	private int vitesse;
+	   
     public Vaisseau(int longueur, int hauteur) {
 	    this(longueur, hauteur, 0, 0);
     }
@@ -16,6 +17,7 @@ public class Vaisseau {
     public Vaisseau(Dimension dimension, Position positionOrigine) {
 	    this.dimension = dimension;
 	    this.origine = positionOrigine;
+	    this.vitesse=1;
     }
 
     public boolean occupeLaPosition(int x, int y) {
@@ -47,11 +49,11 @@ public class Vaisseau {
    }
 
    public void seDeplacerVersLaDroite() {
-	   this.origine.changerAbscisse(this.origine.abscisse() + 1);
+	   this.origine.changerAbscisse(this.origine.abscisse() + vitesse);
    }
 
    public void seDeplacerVersLaGauche() {
-	   this.origine.changerAbscisse(this.origine.abscisse() - 1);
+	   this.origine.changerAbscisse(this.origine.abscisse() - vitesse);
    }
 
    public void positionner(int x, int y) {
