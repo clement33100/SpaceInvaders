9d505c3d849b441185f0d5e0ff1446504bae3a4e
Fougeret <clement.fougeret@etu.unilim.fr>
2019-05-23 08:21:49 +0200

    Vaisseau hérite de Sprite

--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -1,11 +1,7 @@
 package fr.unilim.iut.spaceinvaders;
 
-public class Vaisseau {
+public class Vaisseau extends Sprite {
 
-    private Position origine;
-    private Dimension dimension;
-	private int vitesse;
-	   
     public Vaisseau(int longueur, int hauteur) {
 	    this(longueur, hauteur, 0, 0);
     }
@@ -21,55 +17,6 @@ public class Vaisseau {
     }
     public Vaisseau(Dimension dimension, Position positionOrigine) {
 		this(dimension, positionOrigine, 1);
-	}
-
-    public boolean occupeLaPosition(int x, int y) {
-	    return estAbscisseCouverte(x) && estOrdonneeCouverte(y);
-    }
-
-    private boolean estOrdonneeCouverte(int y) {
-	   return (ordonneeLaPlusBasse() <= y) && (y <= ordonneeLaPlusHaute());
-    }
-
-    private boolean estAbscisseCouverte(int x) {
-	   return (abscisseLaPlusAGauche() <= x) && (x <= abscisseLaPlusADroite());
-    }
-
-    public int ordonneeLaPlusBasse() {
-	    return this.origine.ordonnee() - this.dimension.hauteur() + 1;
-    }
-
-    public int ordonneeLaPlusHaute() {
-	   return this.origine.ordonnee();
-   }
-
-   public int abscisseLaPlusADroite() {
-	   return this.origine.abscisse() + this.dimension.longueur()- 1;
-   }
-
-   public int abscisseLaPlusAGauche() {
-	   return this.origine.abscisse();
-   }
-
-   public void seDeplacerVersLaDroite() {
-	   this.origine.changerAbscisse(this.origine.abscisse() + vitesse);
-   }
-
-   public void seDeplacerVersLaGauche() {
-	   this.origine.changerAbscisse(this.origine.abscisse() - vitesse);
-   }
-
-   public void positionner(int x, int y) {
-	   this.origine.changerAbscisse(x);
-	   this.origine.changerOrdonnee(y);
-   }
-   
-   public int hauteur() {
-		return this.dimension.hauteur();
-	}
-
-	public int longueur() {
-		return this.dimension.longueur();
 	}
 
 }
